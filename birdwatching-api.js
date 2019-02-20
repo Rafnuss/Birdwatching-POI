22672b8b927bcc5c35f8eeee353ccc1d8769d3f8
rafnuss <rafnuss@gmail.com>
2019-02-20 22:23:55 +0100

    update

--- birdwatching-api.js
+++ birdwatching-api.js
@@ -1,5 +1,9 @@
+console.log('hello')
+
 jQuery(document).ready(function(){
     //jQuery('#wpadminbar').hide()
     jQuery('#avada-stylesheet-inline-css').remove()
     jQuery('#avada-parent-stylesheet-css').remove()
 });
+
+
